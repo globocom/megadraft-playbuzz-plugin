dedbc3226448d879659375e83da8baf39d9d8e36
Artur Sousa <artur.sousa@corp.globo.com>
2017-03-16 12:07:11 -0300

    Add a button to change state

--- src/Block.js
+++ src/Block.js
@@ -5,10 +5,7 @@
  */
 
 import React, {Component} from "react";
-
 import {MegadraftPlugin, MegadraftIcons} from "megadraft";
-
-
 const {BlockContent, BlockData, BlockInput, CommonBlock} = MegadraftPlugin;
 
 
@@ -16,35 +13,60 @@ export default class Block extends Component {
   constructor(props) {
     super(props);
 
-    this._handleCaptionChange = ::this._handleCaptionChange;
-    this._handleEdit = ::this._handleEdit;
+    this._onChangeInput = ::this._onChangeInput;
+    this.load = ::this.load;
 
     this.actions = [
-      {"key": "edit", "icon": MegadraftIcons.EditIcon, "action": this._handleEdit},
-      {"key": "delete", "icon": MegadraftIcons.DeleteIcon, "action": this.props.container.remove}
+      {
+        "key": "delete",
+        "icon": MegadraftIcons.DeleteIcon,
+        "action": this.props.container.remove
+      }
     ];
+
+    this.state = {
+      url: (props.data.url) ? props.data.url : "",
+      input: {
+        url: (props.data.url) ? props.data.url : "",
+        errors: []
+      }
+    };
   }
 
-  _handleEdit() {
-    alert(JSON.stringify(this.props.data, null, 4));
+  _onChangeInput(field, e) {
+    let input = this.state.input;
+    input[field] = e.target.value;
+    this.setState({ input: input });
   }
 
-  _handleCaptionChange(event) {
-    this.props.container.updateEntity({caption: event.target.value});
+  load() {
+    if (!this.state.input.url) {
+      return;
+    }
+    this.setState({
+      url: this.state.input.url,
+      input: {
+        url: ""
+      }
+    });
   }
 
   render(){
     return (
       <CommonBlock {...this.props} actions={this.actions}>
         <BlockContent>
-          <pre>{this.props.data.caption || "- NO TEXT -"}</pre>
+          <pre>{this.state.url || "- NO URL -"}</pre>
         </BlockContent>
 
         <BlockData>
           <BlockInput
-            placeholder="Caption"
-            value={this.props.data.caption}
-            onChange={this._handleCaptionChange} />
+            placeholder="Enter a playbuzz URL"
+            value={(this.state.url) ? this.state.url : null}
+            onChange={this._onChangeInput.bind(this, "url")} />
+        </BlockData>
+
+        <BlockData>
+          <button onClick={this.load}>Load</button>
         </BlockData>
       </CommonBlock>
     );
